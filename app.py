c194a75efe3da3de706d4c94e7b5d99c9eef2290
Le36 <48809753+Le36@users.noreply.github.com>
2022-01-25 23:26:34 +0200

    login page

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from os import getenv
+
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
 import routes
--- routes.py
+++ routes.py
@@ -1,8 +1,22 @@
 from app import app
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session
 from db import db
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        return redirect("/")
+
+
+@app.route("/logout")
+def logout():
+    return redirect("/")
