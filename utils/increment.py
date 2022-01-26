c619ae1ed34d043e8eb3d64253164c2d817c98a8
Le36 <48809753+Le36@users.noreply.github.com>
2022-01-26 06:45:59 +0200

    refactor view incrementer

--- utils/increment.py
+++ utils/increment.py
@@ -2,9 +2,6 @@ from db import db
 
 
 def increment(paste_id):
-    sql = "SELECT pastes FROM pastes WHERE pasteid=:pasteid"
-    result = db.session.execute(sql, {"pasteid": paste_id})
-    fetched = result.fetchone()
     sql = "UPDATE pastes SET views = views + 1 WHERE pasteid=:pasteid"
     db.session.execute(sql, {"pasteid": paste_id})
     db.session.commit()
