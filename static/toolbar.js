26e53aae0d73d05b9be4f564eaead858bd0b7dac
Le36 <48809753+Le36@users.noreply.github.com>
2022-02-17 23:00:28 +0200

    paste data with js

--- static/toolbar.js
+++ static/toolbar.js
@@ -1,7 +1,22 @@
 Prism.plugins.toolbar.registerButton("view-raw", function (t) {
-    var e = t.element.parentNode;
-    if (e && /pre/i.test(e.nodeName) && e.hasAttribute("data-src")) {
-        var n = e.getAttribute("data-src"), a = document.createElement("a");
-        return a.textContent = "View raw", a.href = n, a
+    let e = t.element.parentNode;
+    if (e && /pre/i.test(e.nodeName)) {
+        if (e.hasAttribute("data-src")) {
+            let n = e.getAttribute("data-src"), a = document.createElement("a");
+            return a.textContent = "View raw", a.href = n, a
+        }
     }
+});
+
+Prism.plugins.toolbar.registerButton("total-views", {
+    text: "Views: " + views
+});
+
+Prism.plugins.toolbar.registerButton("creator", function (t) {
+    let n = "/u/" + creator, a = document.createElement("a");
+    return a.textContent = "Author: " + creator, a.href = n, a
+});
+
+Prism.plugins.toolbar.registerButton("date", {
+    text: date
 });
