ea8cdf283dfe277128c6995b4be245e2e927316a
Le36 <48809753+Le36@users.noreply.github.com>
2022-02-19 21:36:21 +0200

    fix for tab button on paste form

---
+++ static/tab.js
@@ -0,0 +1,13 @@
+document.getElementById("textbox").addEventListener("keydown", function (e) {
+    if (e.key === "Tab") {
+        e.preventDefault();
+        let start = this.selectionStart;
+        let end = this.selectionEnd;
+
+        // set textarea value to: text before caret + tab + text after caret
+        this.value = this.value.substring(0, start) + "\t" + this.value.substring(end);
+
+        // put caret at right position again
+        this.selectionStart = this.selectionEnd = start + 1;
+    }
+});
